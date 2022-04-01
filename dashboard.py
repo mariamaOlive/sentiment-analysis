8900f5a9310a7480815bb6f5f9a1fd4e350e75b0
Mariama Oliveira <mariama.oliveira@yahoo.com.br>
2022-04-01 10:34:28 -0300

    Preparando dashboard para apresentação
    Co-authored-by: lucascouri2 <lucascouri2@users.noreply.github.com>

--- dashboard.py
+++ dashboard.py
@@ -30,24 +30,20 @@ def load_pred():
 @st.cache
 def load_rep():
     df = pd.read_csv("reviews_rep.csv")
+    df
     return df
 
-# @st.cache
-# def load_wordcloud(data):
-#     lista_rev = data["reviews"].tolist()
-#     big_string = (" ").join(lista_rev)
-#     stop_words = stopwords.words('portuguese')
-#     wordcloud = WordCloud(stopwords=stop_words, background_color="white", max_words=500, contour_width=3, width=1600, height=800).generate(big_string)
-#     fig = plt.figure(figsize=[20,10])
-#     plt.imshow(wordcloud, interpolation='bilinear')
-#     plt.axis("off")
-#     return fig
+@st.cache
+def load_metrics():
+    df = pd.read_csv("metrics.csv").drop("predictions", axis=1)
+    return df
 
+#Loading data
 data_load_state = st.text('Loading data...')
 data = load_data()
 data_pred = load_pred()
 data_rep = load_rep()
-# wordcloud = load_wordcloud(data)
+data_metrics = load_metrics()
 data_load_state.text("Done! (using st.cache)")
 
 # st.subheader('Raw data')
@@ -56,10 +52,14 @@ data_load_state.text("Done! (using st.cache)")
 col1, col2 = st.columns(2)
 
 ### Plot Time Series - Reviews ###
-# st.subheader('Número de reviews')
 col1.header("Reviews no tempo - Original (todos os dados)")
 df_count_reviews = pd.DataFrame({'count' : data.groupby( [ "dates", "class"] ).size()}).reset_index()
-fig = px.line(df_count_reviews, x="dates", y="count", color='class',
+fig = px.line(df_count_reviews, x="dates", y="count", 
+              color='class', 
+                color_discrete_map={
+                 "Positiva": "#00FF00",
+                 "Negativa": "red"
+                },
                 labels={
                         "dates": "Data",
                         "count": "Número de reviews",
@@ -68,22 +68,27 @@ fig = px.line(df_count_reviews, x="dates", y="count", color='class',
                         title="Reviews no tempo")
 col1.plotly_chart(fig)
 
-col2.header("Reviews no tempo - Predições (conjunto de teste)")
+col2.header("Reviews no tempo - Predições do BERT (conjunto de teste)")
 df_count_reviews = pd.DataFrame({'count' : data_pred.groupby( [ "dates", "pred"] ).size()}).reset_index()
-fig2 = px.line(df_count_reviews, x="dates", y="count", color='pred',
+fig2 = px.line(df_count_reviews, x="dates", y="count", 
+                color='pred', 
+                color_discrete_map={
+                 "Positiva": "#00FF00",
+                 "Negativa": "red"
+                },
                 labels={
                         "dates": "Data",
                         "count": "Número de reviews",
                         "pred": "Tipo de review"
                         },
                         title="Reviews no tempo")
+
+fig2.update_layout(legend_traceorder="reversed")
 col2.plotly_chart(fig2)
 
 col3, col4 = st.columns(2)
 
 ### Plot Wordcloud ###
-# st.subheader('Wordcloud')
-#st.header("Wordcloud")
 col3.header("Wordcloud")
 data_load_state = st.text('Loading data...')
 lista_rev = data["reviews"].tolist()
@@ -93,42 +98,19 @@ wordcloud = WordCloud(stopwords=stopwords, background_color="white", max_words=2
 fig3 = plt.figure(figsize=[20,10])
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-#st.pyplot(fig3)
 col3.pyplot(fig3)
 data_load_state.text("")
 
-#st.header("Reviews mais representativos")
-#st.write(data_rep)
 
-#st.header("Reviews mais representativos")
-#st.subheader("Negativos")
-#st.write(data_rep["negativo"][0])
-#st.write(data_rep["negativo"][1])
-#st.subheader("Positivos")
-#st.write(data_rep["positivo"][0])
-#st.write(data_rep["positivo"][1])
+#Review mais representativos
 col4.header("Reviews mais representativos")
 col4.subheader("Negativos")
-col4.write(data_rep["negativo"][0])
-col4.write(data_rep["negativo"][1])
+col4.markdown('- '+ data_rep["negativo"][0])
+col4.markdown('- '+ data_rep["negativo"][1])
 col4.subheader("Positivos")
-col4.write(data_rep["positivo"][0])
-col4.write(data_rep["positivo"][1])
+col4.markdown('- '+ data_rep["positivo"][0])
+col4.markdown('- '+ data_rep["positivo"][1])
 
-
-# hist_values = np.histogram(
-#     #data[""].dt.hour, bins=24, range=(0,24))[0]
-#     data["stars"], bins=24)[0]
-# st.bar_chart(hist_values)
-
-
-#st.line_chart(data[""])
-
-#values = st.sidebar.slider(“Price range”, float(df.price.min()), 1000., (50., 300.))
-#f = px.histogram(df.query(f”price.between{values}”), x=”price”, nbins=15, title=”Price distribution”)
-#f.update_xaxes(title=”Price”)
-#f.update_yaxes(title=”No. of listings”)
-#st.plotly_chart(f)
-
-
-
+#Métricas dos classificadores
+st.header("Métricas dos classificadores")
+st.table(data_metrics)
