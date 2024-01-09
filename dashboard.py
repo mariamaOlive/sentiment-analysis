5d5edbe7fbc131710030839bcc62713cf378144e
Mariama Oliveira <mariama.oliveira@yahoo.com.br>
2024-01-10 00:02:15 +0200

    Translating dashboard

--- dashboard.py
+++ dashboard.py
@@ -4,15 +4,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-from nltk.corpus import stopwords
 
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
 
 # Set page title
-st.title('Reviews da Amazon - Firestick TV')
+st.title('Amazon Reviews - Firestick TV')
+st.caption("This dashboard was created to display the outcomes of a sentiment \
+           analysis study conducted on the Amazon Firestick reviews. The primary \
+           objective of the study was to categorize reviews as either positive or \
+           negative. Moreover, we aimed to identify the most representative reviews\
+           of each category and the most frequently used words in those reviews. \
+           To achieve this, all data has been preprocessed beforehand, and classifiers\
+           such as Random Forest and BERT have been explored. Some interesting findings can be found bellow.")
 
-@st.cache
+@st.cache_data
 def load_data():
     df = pd.read_csv("data/reviews_v2.csv")
     df = df[df["reviews"].notna()]
@@ -20,23 +26,30 @@ def load_data():
     df["dates"] = pd.to_datetime(df["dates"])
     return df
 
-@st.cache
+@st.cache_data
 def load_pred():
     df = pd.read_csv("data/best_pred.csv")
     df["pred"] = df["pred"].apply(lambda x : "Positiva" if x == 1 else "Negativa")
     df["dates"] = pd.to_datetime(df["dates"])
     return df
 
-@st.cache
+@st.cache_data
 def load_rep():
     df = pd.read_csv("data/reviews_rep.csv")
-    df
     return df
 
-@st.cache
+@st.cache_data
 def load_metrics():
     df = pd.read_csv("data/metrics.csv").drop("predictions", axis=1)
     return df
+
+@st.cache_data
+def load_stopword():
+    df = pd.read_csv("portuguese_stopwords.txt", header=None, names=['Words'])
+    stopwords = df['Words'].tolist()
+    stopwords = [i.replace(' ', '') for i in stopwords] 
+    return stopwords
+
 
 #Loading data
 data_load_state = st.text('Loading data...')
@@ -44,73 +57,79 @@ data = load_data()
 data_pred = load_pred()
 data_rep = load_rep()
 data_metrics = load_metrics()
-data_load_state.text("Done! (using st.cache)")
+pt_stopwords = load_stopword()
+data_load_state.text("Data Loaded!")
 
-# st.subheader('Raw data')
-# st.write(data)
 
-col1, col2 = st.columns(2)
 
+### Cloud and most representative - Reviews ###
+col3, col4 = st.columns(2 , gap="large")
+
+with col3:
+    ### Plot Wordcloud ###
+    st.header("Wordcloud")
+    data_load_state = st.text('Loading data...')
+    lista_rev = data["reviews"].tolist()
+    big_string = " ".join(lista_rev)
+    wordcloud = WordCloud(stopwords=pt_stopwords, background_color="white", max_words=200, contour_width=3, width=800, height=400).generate(big_string)
+    st.image(wordcloud.to_array())
+    data_load_state.text("")
+
+with col4:
+    #Review mais representativos
+    st.header("More representative reviews")
+    st.subheader("Negative")
+    st.markdown('- '+ data_rep["negativo"][0])
+    st.markdown('- '+ data_rep["negativo"][1])
+    st.subheader("Positive")
+    st.markdown('- '+ data_rep["positivo"][0])
+    st.markdown('- '+ data_rep["positivo"][1])
+
+st.text("")
+st.text("")
+st.text("")
 ### Plot Time Series - Reviews ###
-col1.header("Reviews no tempo - Original (todos os dados)")
-df_count_reviews = pd.DataFrame({'count' : data.groupby( [ "dates", "class"] ).size()}).reset_index()
-fig = px.line(df_count_reviews, x="dates", y="count", 
-              color='class', 
-                color_discrete_map={
-                 "Positiva": "#00FF00",
-                 "Negativa": "red"
-                },
-                labels={
-                        "dates": "Data",
-                        "count": "Número de reviews",
-                        "class": "Tipo de review"
-                        },
-                        title="Reviews no tempo")
-col1.plotly_chart(fig)
+col1, col2 = st.columns(2, gap="large")
 
-col2.header("Reviews no tempo - Predições do BERT (conjunto de teste)")
-df_count_reviews = pd.DataFrame({'count' : data_pred.groupby( [ "dates", "pred"] ).size()}).reset_index()
-fig2 = px.line(df_count_reviews, x="dates", y="count", 
-                color='pred', 
-                color_discrete_map={
-                 "Positiva": "#00FF00",
-                 "Negativa": "red"
-                },
-                labels={
-                        "dates": "Data",
-                        "count": "Número de reviews",
-                        "pred": "Tipo de review"
-                        },
-                        title="Reviews no tempo")
+with col1:
+    st.header("Reviews in time - Original (all the data)")
+    df_count_reviews = pd.DataFrame({'count' : data.groupby( [ "dates", "class"] ).size()}).reset_index()
+    fig = px.line(df_count_reviews, x="dates", y="count", 
+                color='class', 
+                    color_discrete_map={
+                    "Positiva": "#00FF00",
+                    "Negativa": "red"
+                    },
+                    labels={
+                            "dates": "Date",
+                            "count": "Number of reviews",
+                            "class": "Review Type"
+                            },
+                            title="Reviews in time")
+    st.plotly_chart(fig)
 
-fig2.update_layout(legend_traceorder="reversed")
-col2.plotly_chart(fig2)
+with col2:
+    st.header("Reviews in time - BERT prediction (test)")
+    df_count_reviews = pd.DataFrame({'count' : data_pred.groupby( [ "dates", "pred"] ).size()}).reset_index()
+    fig2 = px.line(df_count_reviews, x="dates", y="count", 
+                    color='pred', 
+                    color_discrete_map={
+                    "Positiva": "#00FF00",
+                    "Negativa": "red"
+                    },
+                    labels={
+                            "dates": "Date",
+                            "count": "Number of reviews",
+                            "class": "Review Type"
+                            },
+                            title="Reviews in time")
 
-col3, col4 = st.columns(2)
-
-### Plot Wordcloud ###
-col3.header("Wordcloud")
-data_load_state = st.text('Loading data...')
-lista_rev = data["reviews"].tolist()
-big_string = (" ").join(lista_rev)
-stopwords = stopwords.words('portuguese')
-wordcloud = WordCloud(stopwords=stopwords, background_color="white", max_words=200, contour_width=3, width=800, height=400).generate(big_string)
-fig3 = plt.figure(figsize=[20,10])
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-col3.pyplot(fig3)
-data_load_state.text("")
+    fig2.update_layout(legend_traceorder="reversed")
+    st.plotly_chart(fig2)
 
 
-#Review mais representativos
-col4.header("Reviews mais representativos")
-col4.subheader("Negativos")
-col4.markdown('- '+ data_rep["negativo"][0])
-col4.markdown('- '+ data_rep["negativo"][1])
-col4.subheader("Positivos")
-col4.markdown('- '+ data_rep["positivo"][0])
-col4.markdown('- '+ data_rep["positivo"][1])
+
 
 #Métricas dos classificadores
-st.header("Métricas dos classificadores")
+st.header("Classifiers' Metrics")
 st.table(data_metrics)
